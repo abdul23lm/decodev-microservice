3528f7e738fce3554a0727276a2736b2145ea4a4
Abdul Latif Munjiat <58551842+abdul23lm@users.noreply.github.com>
2021-02-27 23:38:25 +0700

    Update media.js

--- api-gateway/routes/media.js
+++ api-gateway/routes/media.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 const { APP_NAME } = process.env;
 
-
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('media');
+});
 
 module.exports = router;
